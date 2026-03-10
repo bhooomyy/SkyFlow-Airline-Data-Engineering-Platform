c36b0f88f903959465fc9991049fd284e1d3e9e0
bhooomyy <bhattbhoomi0111@gmail.com>
2026-03-10 18:03:13 -0400

    schema.sql updated
    - dim_airport Table created with appropriate schema.

--- schema.sql
+++ schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE dim_airline(
 		airline_code VARCHAR(5),
 		iata_code VARCHAR(5)
 );
+
+CREATE TABLE dim_airport(
+		airport_id INTEGER,
+        airport_code VARCHAR(5),
+        city_name VARCHAR(30),
+        state_code VARCHAR(5),
+        state_name VARCHAR(20)
+);
