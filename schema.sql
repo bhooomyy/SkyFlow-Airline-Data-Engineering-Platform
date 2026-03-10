8499b45b0559e3c833e578883d37e1e85dd59271
bhooomyy <bhattbhoomi0111@gmail.com>
2026-03-10 18:09:58 -0400

    schema.sql updated
    - empty fact_flghts created with appropriate columns.

--- schema.sql
+++ schema.sql
@@ -22,3 +22,33 @@ CREATE TABLE dim_date(
         day_of_month INTEGER,
         day_of_week INTEGER
 );
+
+CREATE TABLE fact_flights(
+		flight_date DATE,
+        airline_id INTEGER,
+        flight_number INTEGER,
+        tail_number VARCHAR(6),
+        origin_airport_id INTEGER,
+        dest_airport_id INTEGER,
+        scheduled_departure_time INTEGER,
+        actual_departure_time FLOAT,
+        departure_delay FLOAT,
+        departure_delay_minutes FLOAT,
+        scheduled_arrival_time INTEGER,
+        actual_arrival_time FLOAT,
+        arrival_delay FLOAT,
+        arrival_delay_minutes FLOAT,
+        cancelled FLOAT,
+        diverted FLOAT,
+        taxi_out FLOAT,
+        taxi_in FLOAT,
+        air_time FLOAT,
+        distance FLOAT,
+        carrier_delay FLOAT,
+        weather_delay FLOAT,
+        nas_delay FLOAT,
+        security_delay FLOAT,
+        late_aircraft_delay FLOAT
+);
+
+
